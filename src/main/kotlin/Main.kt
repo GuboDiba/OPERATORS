8aa4ef4b3f25467900abd98d3847b6b5e79155db
Gubo Diba <gubodibawario@gmail.com>
2023-02-09 07:55:02 +0300

    kortlin codes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(){
+name()
+    reminder()
+    sum(56, 67, 65, 89)
+    fact()
+}
+fun name(){
+    var name="Gubo"
+     println("Hello" + " " + name)
+}
+fun reminder(){
+    var a = 78
+    var b = 17
+    var sum = a % b
+    println(sum)
+}
+fun sum (num1:Int, num2:Int, num3:Int, num4:Int){
+    var sum = num1 + num2 + num3 + num4
+    println(sum)
+}
+fun fact(){
+    var fact = "i am tall"
+    println(fact)
+}
